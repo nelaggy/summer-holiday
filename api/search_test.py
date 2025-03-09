d30ab045e32df04bd8004cf22207d30f5ac04e6b
Coy Zhu <ckz20@cam.ac.uk>
2025-03-09 15:33:19 +0000

    created test file

--- api/search_test.py
+++ api/search_test.py
@@ -1,12 +1,6 @@
-from openai import OpenAI
+from search import search
 
+sample_input = {"place"  : "Italy", "checkin_date"  : "2025-06-16", "checkout_date"  : "2025-06-19", "adults_number"  : 5, "children_number"  : 1, "dyanamic_filters" : {"toilets" : "yes", "non-smoking" : "yes", "swimming pool" : "yes"}}
 
-completion = client.chat.completions.create(
-  model="gpt-3.5-turbo",
-  store=True,
-  messages=[
-    {"role": "user", "content": "write a haiku about ai"}
-  ]
-)
+output = search(sample_input, full_results = True)
 
-print(completion.choices[0].message)
