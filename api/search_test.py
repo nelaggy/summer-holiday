903890a9d2937be09a757c3d78f1a360bb35f064
Coy Zhu <ckz20@cam.ac.uk>
2025-03-09 16:43:18 +0000

    edits

--- api/search_test.py
+++ api/search_test.py
@@ -4,5 +4,3 @@ import asyncio
 sample_input = {"place"  : "Germany", "budget"  : 1000.00,"checkin_date"  : "2025-06-16", "checkout_date"  : "2025-06-19", "adults_number"  : 5, "children_number"  : 1, "dyanamic_filters" : {"non-smoking" : "yes", "swimming pool" : "yes"}}
 
 output = asyncio.run(search(sample_input, full_results = True))
-
-print(output)
