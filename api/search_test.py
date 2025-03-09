fe1304f3d87a3685cde5608d5cac9baf7bc7965b
Coy Zhu <ckz20@cam.ac.uk>
2025-03-09 15:21:49 +0000

    removed api key

--- api/search_test.py
+++ api/search_test.py
@@ -1,8 +1,5 @@
 from openai import OpenAI
 
-client = OpenAI(
-  api_key="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"
-)
 
 completion = client.chat.completions.create(
   model="gpt-3.5-turbo",
